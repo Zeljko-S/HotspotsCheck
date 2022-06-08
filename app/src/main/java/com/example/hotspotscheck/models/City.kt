ba05ca845b32ed50dcadd2171fb63a90ba228c9f
Zeljko Sekulic <zeljko.sekulic@stud-fh-campuswien.ac.at>
2022-06-08 11:38:33 +0200

    Commit

--- app/src/main/java/com/example/hotspotscheck/models/City.kt
+++ app/src/main/java/com/example/hotspotscheck/models/City.kt
@@ -18,7 +18,7 @@ fun getCities(): List<City> {
             "https://blog.playandtour.com/wp-content/uploads/2017/03/nueva-york-central-park-zoo-delacorte-reloj.jpg",
             "https://afar-production.imgix.net/uploads/images/post_images/images/yLyRK7tDDk/original_9678f51ddef311fd9936ceea4ba264c0.jpg?1549376797?ixlib=rails-0.3.0&auto=format%2Ccompress&crop=entropy&fit=crop&h=719&q=80&w=954"
             ),
-            hotstopnamelist = listOf()
+            hotstopnamelist = listOf("Empire State Building", "Top of the Rock", "Circle Line Cruise", "Edge", "Central Park Zoo", "NY Aquarium")
 
         ),
         City(
@@ -32,7 +32,7 @@ fun getCities(): List<City> {
             "https://imgsrv2.voi.id/AqJDpiqhkIXkbcmeg2mDmEscRW2zCvy25JcGA0BO7A0/auto/1280/853/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy82NTk1LzIwMjAwNTMxMTUzNC1tYWluLmNyb3BwZWRfMTU5MDkxNDA5NC5qcGVn.jpg",
             "https://images-ext-2.discordapp.net/external/Eftr5986QhMF5EURR_Rl3vhpzX72pCBT7k5bqW1EoOM/https/mywowo.net/media/images/cache/londra_hyde_park_03_kensington_gardens_jpg_1200_630_cover_85.jpg?width=1166&height=612",
             ),
-            hotstopnamelist = listOf()
+            hotstopnamelist = listOf("Trafalgar Square",  "Buckingham Palace", "Natural History Museum", "Tower Bridge", "Big Ben", "Hyde Park")
         ),
         City(
             id = "3",
@@ -45,7 +45,7 @@ fun getCities(): List<City> {
             "https://www.gotokyo.org/de/story/guide/shinjuku-shop/images/shopping_guide.jpg",
             "https://img.fotocommunity.com/sensoji-tempel-tokyo-a5dfad3c-6ffe-4ef3-ac70-2089acf5c6ce.jpg?height=1080"
             ),
-            hotstopnamelist = listOf()
+            hotstopnamelist = listOf("Meguro River", "Yoyogi Park", "Chidorigafuchi", "Shinjuku Central Park", "Shinjuku", "Sensō-ji Tempel")
         )
 
 
