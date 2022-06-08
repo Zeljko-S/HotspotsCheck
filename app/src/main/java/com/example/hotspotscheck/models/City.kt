4e075ca97cd2448ad220c9e0b315fcbfdfe0f941
FHAleex <alex.babakhel@stud.fh-campuswien.ac.at>
2022-06-08 11:34:10 +0200

    gridlayout finish

--- app/src/main/java/com/example/hotspotscheck/models/City.kt
+++ app/src/main/java/com/example/hotspotscheck/models/City.kt
@@ -1,8 +1,8 @@
 package com.example.hotspotscheck.models
 
-data class City(val id: String, val name: String, val img: String)
+data class City(val id: String, val name: String, val img: String, val hotspotimgs: List<String>, val hotstopnamelist: List<String>)
 
-data class Hotstop(val name: String, val img: String)
+//data class Hotstop(val name: String, val img: String)
 
 
 fun getCities(): List<City> {
@@ -10,18 +10,44 @@ fun getCities(): List<City> {
         City(
             id = "1",
             name = "New York",
-            img = "https://www.godominicanrepublic.com/wp-content/uploads/2018/03/newyork-64819822_xxl.jpg"
+            img = "https://www.godominicanrepublic.com/wp-content/uploads/2018/03/newyork-64819822_xxl.jpg",
+            hotspotimgs = listOf("https://marvel-b1-cdn.bc0a.com/f00000000179470/www.esbnyc.com/sites/default/files/styles/small_feature/public/2019-10/home_banner-min.jpg?itok=uZt-03Vw",
+            "https://media02.stockfood.com/largepreviews/MjE4MTA5NDM3Mw==/70357883-Tourists-Admiring-the-Panoramic-View-Over-Manhattan-From-the-Top-of-the-Rock-Observation-Deck-in-the-Ge.jpg",
+            "https://cdn.getyourguide.com/img/tour/0dfe187f32955809.jpeg/146.jpg",
+            "https://cdn.prod.www.spiegel.de/images/48f82c15-838c-4855-819a-ae641d8ca17b_w1200_r1.33_fpx72.57_fpy50.jpg",
+            "https://blog.playandtour.com/wp-content/uploads/2017/03/nueva-york-central-park-zoo-delacorte-reloj.jpg",
+            "https://afar-production.imgix.net/uploads/images/post_images/images/yLyRK7tDDk/original_9678f51ddef311fd9936ceea4ba264c0.jpg?1549376797?ixlib=rails-0.3.0&auto=format%2Ccompress&crop=entropy&fit=crop&h=719&q=80&w=954"
+            ),
+            hotstopnamelist = listOf()
+
         ),
         City(
             id = "2",
             name = "London",
-            img = "https://germanbackpacker.com/wp-content/uploads/2018/10/London-Eye-Preise-Erfahrungen-2-e1539963321177.jpg"
+            img = "https://germanbackpacker.com/wp-content/uploads/2018/10/London-Eye-Preise-Erfahrungen-2-e1539963321177.jpg",
+            hotspotimgs = listOf("https://bitmap.ticketlens.com/poi/265308607/1200x1200",
+            "https://images-ext-1.discordapp.net/external/CpH1ll737YOXvEMIHLr5KGbEFxdGIdxG2MsjaSgElhQ/https/upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Natural_History_Museum_London_Jan_2006.jpg/800px-Natural_History_Museum_London_Jan_2006.jpg",
+            "https://www.p.city-walks.info/London/Buckingham-Palace.jpg",
+            "https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fill,f_auto,fl_progressive,h_533,q_auto,w_800/v1428566434/article/R11641_image1",
+            "https://imgsrv2.voi.id/AqJDpiqhkIXkbcmeg2mDmEscRW2zCvy25JcGA0BO7A0/auto/1280/853/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy82NTk1LzIwMjAwNTMxMTUzNC1tYWluLmNyb3BwZWRfMTU5MDkxNDA5NC5qcGVn.jpg",
+            "https://images-ext-2.discordapp.net/external/Eftr5986QhMF5EURR_Rl3vhpzX72pCBT7k5bqW1EoOM/https/mywowo.net/media/images/cache/londra_hyde_park_03_kensington_gardens_jpg_1200_630_cover_85.jpg?width=1166&height=612",
+            ),
+            hotstopnamelist = listOf()
         ),
         City(
             id = "3",
             name = "Tokio",
-            img = "https://www.falstaff.at/fileadmin/_processed_/7/1/csm_1-tokio-c-GettyImages-2640_22025df6c6.jpg"
+            img = "https://www.falstaff.at/fileadmin/_processed_/7/1/csm_1-tokio-c-GettyImages-2640_22025df6c6.jpg",
+            hotspotimgs = listOf("https://upload.wikimedia.org/wikipedia/commons/8/85/Meguro_River_Spring_2014%282%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/f/f3/Fountain_Yoyogipark.JPG",
+            "https://oddviser.com/photo/place/1600/393.jpg",
+            "https://i.pinimg.com/originals/24/28/e5/2428e5946542ba757c864117a3a2b298.jpg",
+            "https://www.gotokyo.org/de/story/guide/shinjuku-shop/images/shopping_guide.jpg",
+            "https://img.fotocommunity.com/sensoji-tempel-tokyo-a5dfad3c-6ffe-4ef3-ac70-2089acf5c6ce.jpg?height=1080"
+            ),
+            hotstopnamelist = listOf()
         )
+
 
     )
 }
--- app/src/main/java/com/example/hotspotscheck/screens/HotspotScreen.kt
+++ app/src/main/java/com/example/hotspotscheck/screens/HotspotScreen.kt
@@ -1,9 +1,23 @@
 package com.example.hotspotscheck.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.example.hotspotscheck.assets.TopBar
 import com.example.hotspotscheck.models.City
 import com.example.hotspotscheck.models.getCities
@@ -18,12 +32,41 @@ fun HotspotScreen(navController: NavController = rememberNavController(), cityid
         onTopbarClick = {
             navController.navigate(route = Screens.ChecklistScreen.name)
         }) {
-            Text(text = "Some important text")
+           // Text(text = "Some important text")
+            Hotspots(city.hotspotimgs)
         }
 }
 
+
+
+
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Hotstops() {
+fun Hotspots(hotspotlist: List<String>) {
+    LazyVerticalGrid(cells = GridCells.Fixed(2), contentPadding = PaddingValues(2.dp)) {
+        items(hotspotlist) { hotspot ->
+            Card(
+                modifier = Modifier.padding(2.dp),
+
+            ) {
+                Box(contentAlignment = Alignment.BottomStart) {
+                    AsyncImage(
+                        model = hotspot,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillWidth
+                    )
+                    Text(
+                        text = "Hallo Zeljko",
+                        color = Color.White,
+                        style = MaterialTheme.typography.h6,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                }
+            }
+        }
+    }
 
 }
 
